16a2d7616e336fb9b35a8a8d06bd07f7faede3d6
scherpablo <scherpablo@gmail.com>
2023-09-14 10:18:24 -0300

    pixel meta

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import Header from './components/Header/Header'
-import SectionOne from './components/SectionOne/SectionOne'
-import SectionTwo from './components/SectionTwo/SectionTwo'
-import SectionThree from './components/SectionThree/SectionThree'
-// import SectionFour from './components/SectionFour/SectionFour'  
-import SectionFive from './components/SectionFive/SectionFive'
-import Footer from './components/Footer/Footer'
+import { Helmet } from "react-helmet";
+import Header from "./components/Header/Header";
+import SectionOne from "./components/SectionOne/SectionOne";
+import SectionTwo from "./components/SectionTwo/SectionTwo";
+import SectionThree from "./components/SectionThree/SectionThree";
+// import SectionFour from './components/SectionFour/SectionFour'
+import SectionFive from "./components/SectionFive/SectionFive";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-
   return (
     <>
+      <Helmet>
+        <script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '277120668505791');
+            fbq('track', 'PageView');
+        `}
+        </script>
+      </Helmet>
       <Header />
       <SectionOne />
       <SectionTwo />
@@ -18,7 +34,7 @@ const App = () => {
       <SectionFive />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
