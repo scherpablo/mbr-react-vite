53f72f8ef249d172db80e02f4603d21b43dd7580
scherpablo <scherpablo@gmail.com>
2023-09-14 17:53:36 -0300

    purchase

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import NavBar from "../NavBar/NavBar";
@@ -15,23 +16,7 @@ const Header = () => {
     <>
       <header className={`${styles.section} ${styles.header}`} id="home">
         <Helmet>
-          {/* <script>
-            {`
-            <script>
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            </script>
-            <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=277120668505791&ev=PageView&noscript=1"
-            /></noscript>
-        `}
-          </script> */}
+          
         </Helmet>
         <div className={styles.divHeader}>
           <NavBar />
