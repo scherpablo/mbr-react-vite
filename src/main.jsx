a0e955e7ec41548d85cc86c07e7257d074bac0cf
scherpablo <scherpablo@gmail.com>
2023-09-18 20:26:49 -0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { inject } from "@vercel/analytics";
-import { Analitycs } from "@vercel/analytics";
 
 inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
-      <Analitycs />
     </BrowserRouter>
   // <React.StrictMode>
   //   <App />
