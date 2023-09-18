a6ef34ecf79a45e2b8ed846cddb525f07cb42c85
scherpablo <scherpablo@gmail.com>
2023-09-18 20:23:16 -0300

    analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 // import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter  } from 'react-router-dom'
-import { inject } from '@vercel/analytics';
- 
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { inject } from "@vercel/analytics";
+import { Analitycs } from "@vercel/analytics/react";
+
 inject();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <Analitycs />
+  </>
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-)
+);
