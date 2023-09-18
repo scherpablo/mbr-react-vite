28a093d0a5d744ad13da35734ca7e778997147b3
scherpablo <scherpablo@gmail.com>
2023-09-18 20:24:27 -0300

    analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { inject } from "@vercel/analytics";
-import { Analitycs } from "@vercel/analytics/react";
+import { Analitycs } from "@vercel/analytics";
 
 inject();
 
