b4b22bff121947b2b5de4bdb06cbbb2312f7541f
scherpablo <scherpablo@gmail.com>
2023-09-19 09:29:08 -0300

    footer hidden

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SectionTwo from "./components/SectionTwo/SectionTwo";
 import SectionThree from "./components/SectionThree/SectionThree";
 // import SectionFour from './components/SectionFour/SectionFour'
 import SectionFive from "./components/SectionFive/SectionFive";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
       <SectionThree />
       {/* <SectionFour /> */}
       <SectionFive />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
