ca4f2b61a3171e10f9e50d1d1db915011fb196ae
scherpablo <scherpablo@gmail.com>
2023-09-20 10:47:06 -0300

    comment pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// import React from 'react'
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
@@ -10,7 +9,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
 );
